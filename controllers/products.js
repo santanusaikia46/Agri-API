dc19bd49fc6c835f7621429998e4e76b085519d5
SANTANU SAIKIA <87136299+santanusaikia46@users.noreply.github.com>
2024-03-28 23:45:37 +0530

    tt

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const products = require("../models/products");
 
 const getAllProducts = async (req, res) => {
-    const Products = await products.find(req.query)
+    const Products = await Product.find(req.query)
     res.status(200).json({ Products });
 };
 const getAllProductsTesting = async (req, res) => {
